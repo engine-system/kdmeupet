521896bad330880e67e8b1aadca05167f9a090e6
Denis <deniscarvasilva@gmail.com>
2018-04-29 09:46:08 -0300

    inclusao de tela de raças

--- src/pages/cadastro-pet-3/cadastro-pet-3.ts
+++ src/pages/cadastro-pet-3/cadastro-pet-3.ts
@@ -3,30 +3,53 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CadastroPet_4Page } from '../cadastro-pet-4/cadastro-pet-4';
 import { CadastroPet_2Page } from '../cadastro-pet-2/cadastro-pet-2';
 
-/**
- * Generated class for the CadastroPet_3Page page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-cadastro-pet-3',
   templateUrl: 'cadastro-pet-3.html',
 })
 export class CadastroPet_3Page {
-
+  racas: any[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.racas = [{
+      codigo: '01',
+      nome: 'Border Collie'
+    }, {
+      codigo: '02',
+      nome: 'Chiu au Á'
+    }
+    ];
+  }
+
+  getItems(ev: any) {
+    this.initializeItems();
+
+    let val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.racas = this.racas.filter((item) => {
+        return (item['nome'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  clicou(texto) {
+    console.log(texto);
+  }
+  onCancel(ev: any) {
+    console.log("cancelado");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastroPet_3Page');
   }
-  public proximo(){
+  public proximo() {
     this.navCtrl.push(CadastroPet_4Page);
   }
-  public anterior(){
+  public anterior() {
     this.navCtrl.push(CadastroPet_2Page);
   }
 
