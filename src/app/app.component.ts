64b47aab9eebe319239516c0893949ca919455a4
Denis <deniscarvasilva@gmail.com>
2018-06-01 02:08:45 -0300

    tela de splash timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       //splashScreen.show();
-      //splashScreen.hide();
+      splashScreen.hide();
 
     });
   }
